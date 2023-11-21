0078f8e4b7ebc2460af27c5f1edcff0812e5cd93
Elia <e282240@edu.sbl.ch>
2023-11-21 09:50:24 +0100

    Keine "e"s mehr

--- script.js
+++ script.js
@@ -2,9 +2,32 @@ export function aufgabe01(args) {
   const input = args
   const result = []
 
+   
+
   for (let i = 0; i < input.length; i++) {
     const currentElement = input[i]
+    if (currentElement != "e") {
     result.push(currentElement)
   }
+}
   return result.join("")
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
